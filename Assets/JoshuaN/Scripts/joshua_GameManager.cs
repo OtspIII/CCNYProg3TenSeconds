a7bf1692653a46c491a082d30a9cadac16ec70c7
ChillBeanJosh <joshover23@gmail.com>
2024-10-20 18:09:47 -0400

    scene updates

--- Assets/JoshuaN/Scripts/joshua_GameManager.cs
+++ Assets/JoshuaN/Scripts/joshua_GameManager.cs
@@ -12,11 +12,23 @@ public class joshua_GameManager : MonoBehaviour
 
     [Header("UI References")]
     public TextMeshProUGUI healthText; 
-    public TextMeshProUGUI comboText; 
+    public TextMeshProUGUI comboText;
+    public TextMeshProUGUI failText; 
+    public TextMeshProUGUI passText; 
+    public TextMeshProUGUI timerText;
+
+    private float countdownTime = 10f;
+    private bool timerActive = true;
+
+    private GameManager_Misha gameManager;
+
 
     void Start()
     {
+        gameManager = FindObjectOfType<GameManager_Misha>();
+
         UpdateUI();
+        StartCoroutine(Timer());
     }
 
     public void UpdateHealth(int amount)
@@ -26,6 +38,12 @@ public class joshua_GameManager : MonoBehaviour
 
         ResetCombo(); 
         UpdateUI(); 
+
+        if (health <= 0)
+        {
+            timerActive = false;
+            ToggleFailText(true);
+        }
     }
 
     public void ResetCombo()
@@ -51,6 +69,65 @@ public class joshua_GameManager : MonoBehaviour
         {
             comboText.text = "Combo: " + combo + "!!!";
         }
+    }
+
+    public IEnumerator Timer()
+    {
+        while (countdownTime > 0 && timerActive)
+        {
+            countdownTime -= Time.deltaTime;
+
+            UpdateTimerText(countdownTime);
+
+            yield return null; 
+        }
+
+        if (health > 0)
+        {
+            TogglePassText(true); 
+            StartCoroutine(PauseGame(3f));
+        }
+        else
+        {
+            ToggleFailText(true); 
+            StartCoroutine(PauseGame(3f));
+        }
+
+        UpdateTimerText(0);
+    }
+
+    private void UpdateTimerText(float timeRemaining)
+    {
+        if (timerText != null)
+        {
+            timerText.text = "Time Remaining: " + Mathf.Ceil(timeRemaining) + "s"; 
+        }
+    }
+
+    private void ToggleFailText(bool state)
+    {
+        if (failText != null)
+        {
+            failText.gameObject.SetActive(state);
+        }
+    }
+
+    private void TogglePassText(bool state)
+    {
+        if (passText != null)
+        {
+            passText.gameObject.SetActive(state);
+        }
+    }
+
+
+    private IEnumerator PauseGame(float pauseDuration)
+    {
+        Time.timeScale = 0; 
+        yield return new WaitForSecondsRealtime(pauseDuration); 
+        Time.timeScale = 1; 
+
+        StartCoroutine(gameManager.CoroutineCountdown());
     }
 }
 
