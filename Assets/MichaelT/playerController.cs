b64b248457e0b3394ce7f91ea6af848c1ca12406
Michael <thelostkid224@gmail.com>
2024-10-21 18:12:02 -0400

    10 Secs Game

---
+++ Assets/MichaelT/playerController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    //speed is a multiplier used to control actual speed of player
+    public float speed;
+    private Vector3 myDir;
+    int myScore;
+    public gameManager myMgr;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Dir();
+        transform.Translate(Dir());
+    }
+
+    public Vector3 Dir()
+    {
+        //referencing Unity's virtual axis - these pick up KBM OR controller inputs
+        float y = Input.GetAxis("Vertical");
+        float x = Input.GetAxis("Horizontal");
+        myDir = new Vector3(x, y, 0); //combining them into one vector
+        Debug.Log(myDir);
+        return myDir; //return the value
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("other: " + collision.gameObject.name);
+        Debug.Log("other tag: " + collision.gameObject.tag);
+
+        if (collision.gameObject.tag == "Collectible")
+        {
+            Destroy(collision.gameObject);
+            myMgr.EnterFinale();
+            myScore++;
+        }
+    }
+}
+
+
