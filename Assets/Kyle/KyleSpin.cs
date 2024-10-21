26d1eb7c13988c0de2bb3dd52f0d3f0394c3116f
Kyle Yang <kyleyang2020@gmail.com>
2024-10-21 16:53:44 -0400

    added assets

--- Assets/Kyle/KyleSpin.cs
+++ Assets/Kyle/KyleSpin.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using Image = UnityEngine.UI.Image;
 
 public class KyleSpin : MonoBehaviour
 {
@@ -13,32 +16,39 @@ public class KyleSpin : MonoBehaviour
     public TextMeshProUGUI currentWASDText;
     public TextMeshProUGUI currentDegreeText;
 
+    public Sprite bg1;
+    public Sprite bg2;
+    public Sprite bg3;
+    public Sprite bg4;
+
+    public Image bg;
+
     private void Update()
     {
         currentWASDText.text = currentWASD.ToString();
-        currentDegreeText.text = spinDegree.ToString();
+        currentDegreeText.text = "Cranks Left: " + ((7200 - spinDegree)/360).ToString();
 
         if (Input.GetKeyDown(KeyCode.W) && currentWASD == 'W')
         {
-            Debug.Log("WWWWWWWWWWWWW");
+            bg.sprite = bg1;
             spinDegree += perSpinDegree;
             currentWASD = 'A';
         }
         if (Input.GetKeyDown(KeyCode.A) && currentWASD == 'A')
         {
-            Debug.Log("AAAAAAAAAAAA");
+            bg.sprite = bg2;
             spinDegree += perSpinDegree;
             currentWASD = 'S';
         }
         if (Input.GetKeyDown(KeyCode.S) && currentWASD == 'S')
         {
-            Debug.Log("S");
+            bg.sprite = bg3;
             spinDegree += perSpinDegree;
             currentWASD = 'D';
         }
         if (Input.GetKeyDown(KeyCode.D) && currentWASD == 'D')
         {
-            Debug.Log("D");
+            bg.sprite = bg4;
             spinDegree += perSpinDegree;
             currentWASD = 'W';
         }
